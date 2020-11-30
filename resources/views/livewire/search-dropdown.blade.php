db04aee3ceb2ca7b31b49f3e9415f004b7e6a8c1
hasta karya <hasta.it@langitpayment.com>
2020-11-30 16:48:00 +0700

    add focus in search input when press slash keyboard

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,11 @@
 <div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search" @keydown="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" @focus="isOpen = true">
+    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search" @keydown="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" @focus="isOpen = true" x-ref="search" 
+    @keydown.window="
+        if (event.keyCode === 191) {
+            event.preventDefault();
+            $refs.search.focus();
+        }
+    ">
     <div class="absolute top-0">
         <svg class="fill-current text-gray-500 w-4 mt-2 ml-2" viewBox="0 0 515.558 515.558"><path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"/></svg>
     </div>
