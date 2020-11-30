468eeeae56caf9225cb79109e93a02e8c6c5ee1e
hasta karya <hasta.it@langitpayment.com>
2020-11-30 16:42:08 +0700

    add modal for images in movie detail

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -65,7 +65,7 @@
                             </div>
                         @endif
 
-                        <div style="background-color: rgba(0, 0, 0, 0.5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show.transition.opacity="isOpen">
+                        <div style="background-color: rgba(0, 0, 0, 0.5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
                             <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
                                 <div class="bg-gray-900 rounded">
                                     <div class="flex justify-end pr-4 pt-2">
@@ -109,19 +109,34 @@
         </div>
     </div>
 
-    <div class="movie-cast border-b border-gray-800">
+    <div class="movie-cast border-b border-gray-800" x-data="{ isOpen: false, image:'' }">
+    {{-- <div class="movie-images" x-data="{ isOpen: false, image:''}"> --}}
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Image</h2>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 @foreach ($movie['images']['backdrops'] as $image)
                     <div class="mt-8">
-                        <a href="">
+                        <a @click.prevent="isOpen = true, image='{{ 'https://image.tmdb.org/t/p/original'.$image['file_path'] }}'" href="#">
                             <img src="{{ 'https://image.tmdb.org/t/p/w500'.$image['file_path'] }}" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                     </div>
                 @endforeach
             </div>
+
+            <div style="background-color: rgba(0, 0, 0, 0.5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;</button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
+                            <img :src="image" alt="poster">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
 @endsection
